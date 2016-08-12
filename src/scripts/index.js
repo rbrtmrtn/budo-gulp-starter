cb48618a36e17ebf7968e7890d946ee54be6ddab
Robert Martin <rmartin@rmart.in>
2016-08-11 20:51:18 -0400

    Streamline content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,5 @@
-const domify = require('domify')
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
 
-require('domready')(() => {
-  //show canvas demo
-  let context = require('./canvas')()
-  document.body.appendChild(context.canvas)
-
-  //show copy
-  let url = 'https://github.com/mattdesl/budo-gulp-starter'
-  let list = [
-    'npm dependencies with browserify',
-    'incremental bundling with watchify',
-    'SASS for CSS pre-processor',
-    'LiveReload for browser refresh and CSS injection',
-    'Babel for ES6 transpiling',
-    'syntax error reporting with errorify'
-  ].map(x => `<li>${x}</li>`)
-   .join('\n')
-
-  list = domify(`
-    <p>workflow</p>
-    <ul>${list}</ul>
-    <a href="${url}">[source]</a>`)
-  document.body.appendChild(list)
-})
+render(<App />, document.getElementById('main'))
