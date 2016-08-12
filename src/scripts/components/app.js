ac811e43827b26bdbea8f2096d6d7d0317f20355
Robert Martin <rmartin@rmart.in>
2016-08-11 20:50:23 -0400

    Make some React components

---
+++ src/scripts/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './header'
+
+export default class App extends React.Component {
+  render() {
+    return (
+    	<div>
+	    	<Header />
+	    	<p>time to write some code.</p>
+	    	<img src="assets/uc.gif" />
+	    </div>
+    )
+  }
+}
